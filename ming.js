274e0651544c9abb9754acfcf60fbae136fa6d1d
Chris Wong <lambda.fairy@gmail.com>
2015-07-11 03:03:14 -0400

    MONOTONICALLY INCREASING NUMBER GAME

---
+++ ming.js
@@ -0,0 +1,24 @@
+function Ming(display, button) {
+  this.number = 0;
+  this.display = display;
+  this.button = button;
+  this.button.addEventListener('click', function (e) {
+    this.increment();
+    this.update();
+  }.bind(this));
+  this.update();
+}
+
+Ming.prototype.increment = function () {
+  ++this.number;
+};
+
+Ming.prototype.update = function () {
+  this.display.innerHTML = this.number;
+};
+
+document.addEventListener('DOMContentLoaded', function (e) {
+  window.ming = new Ming(
+    document.getElementById('display'),
+    document.getElementById('button'));
+});
